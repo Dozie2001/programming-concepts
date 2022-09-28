ea3c8b93472e0c39056c2ddc7c6ef474d771c702
Dozie2001 <davidchidozie402@gmail.com>
2022-09-28 01:53:31 +0100

    hey

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, new_element):
+    new_list = [i for i in range(len(my_list))]
+
+    if idx < 0 or idx > len(my_list):
+        return  None
+    new_list[idx] = new_element
+
+    return new_list
