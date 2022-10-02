7d986d5b184a31010424c1e85ad0b35c66a5811d
Dozie2001 <davidchidozie402@gmail.com>
2022-10-02 20:56:17 +0100

    New file: search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list =[]
-    if my_list == None:
+    new_list = []
+    if my_list is None:
         return None
     else:
         new_list = my_list[:]
@@ -11,4 +11,3 @@ def search_replace(my_list, search, replace):
             new_list.remove(new_list[x])
             new_list.insert(x, replace)
     return new_list
-    
