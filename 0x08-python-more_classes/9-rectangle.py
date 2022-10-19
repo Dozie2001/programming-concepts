ec19d998f08d5fd20fb8bd678b13946c5829b2aa
Dozie2001 <davidchidozie402@gmail.com>
2022-10-19 23:23:23 +0100

    Hi

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -94,8 +94,6 @@ class Rectangle:
             if i != (self.__height - 1):
                 rect.append('\n')
         return ("".join(rect))
-    
-
 
     def __repr__(self):
         """A formal representation of the class"""
