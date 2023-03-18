33f75fcac81ef451baabf76e5c61861c6f65645c
Dozie2001 <davidchidozie402@gmail.com>
2023-03-18 16:48:37 +0100

    New File

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- A tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+SELECT s.`title` 
+    FROM `tv_shows` AS s
+       INNER JOIN `tv_show_genres` AS t
+           ON s.`id` = t.`show_id`
+        INNER JOIN `tv_genres` AS g
+           ON t.`genre_id` = g.`id`
+    WHERE g.`name` = 'Comedy'
+ORDER BY s.`title` ASC;
