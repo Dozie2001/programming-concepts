f7ab45f922412a8f3d7824959e42f72a0a14c528
Dozie2001 <davidchidozie402@gmail.com>
2023-03-18 18:01:14 +0100

    New File

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,11 @@
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
-SELECT DISTINCT     s.`title`
+SELECT DISTINCT s.`title`
   FROM `tv_shows` AS s
-    INNER JOIN `tv_show_genres` AS t
+    LEFT JOIN `tv_show_genres` AS t
        ON s.id = t.`show_id`
-    INNER JOIN `tv_genres` AS g
+    LEFT JOIN `tv_genres` AS g
       ON t.`show_id` = g.`id`
     WHERE s.`title` NOT IN(
         SELECT s.`title`
@@ -15,7 +15,5 @@ SELECT DISTINCT     s.`title`
                ON s.id = t.`show_id`
             INNER JOIN `tv_genres` AS g
                ON t.`show_id` = g.`id`
-           WHERE g.`name` = "Comedy"
-    )
+           WHERE g.`name` = "Comedy")
   ORDER BY s.`title` ASC; 
-     
