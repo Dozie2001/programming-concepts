0139d4ffffa106e576ef6e54cee17fe5e01ddfd2
Dozie2001 <davidchidozie402@gmail.com>
2023-03-18 11:53:30 +0100

    New File

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- tv_show(genre_id) is foreign key pointing to tv_genres id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id` ASC ;
