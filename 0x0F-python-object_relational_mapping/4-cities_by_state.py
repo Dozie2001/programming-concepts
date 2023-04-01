2fa223c618477d55687d0102cc3a73668aa5646f
Dozie2001 <davidchidozie402@gmail.com>
2023-04-01 19:48:24 +0100

    Code error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db = MySQL.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                        user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
