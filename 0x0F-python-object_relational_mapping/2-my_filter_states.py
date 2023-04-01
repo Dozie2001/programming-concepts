07e9881a11addcd6e46c4f9f1b5de4ad884d073a
Dozie2001 <davidchidozie402@gmail.com>
2023-04-01 16:56:30 +0100

    New file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,6 +5,8 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
+    """lists all states from the database hbtn_0e_0_usa"""""
+
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          password=argv[2], database=argv[3], port=3306)
     cur = db.cursor()
