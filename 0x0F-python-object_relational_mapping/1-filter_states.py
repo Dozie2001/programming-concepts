acbcc4c2e515fb69342a1379890d5eebfcc7bb3b
Dozie2001 <davidchidozie402@gmail.com>
2023-04-01 16:59:45 +0100

    New file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""A  script that lists all states with a name starting with N (upper N)"""
-
+"""   A  script that lists all states with a name starting with N (upper N)"""
 from sys import argv
 import MySQLdb
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""""
-
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    """lists all states from the database hbtn_0e_0_usa"""""
-
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         password=argv[2], database=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY
-                '{}' ORDER BY states.id ASC".format(argv[4]")
-
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
