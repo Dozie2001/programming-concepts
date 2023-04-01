5796773e226ca031b94da4a2748b7fe9034fdcf6
Dozie2001 <davidchidozie402@gmail.com>
2023-04-01 16:36:38 +0100

    New file

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin /python3
+"""A  script that lists all states with a name starting with N (upper N)"""
+
+from sys import argv
+import MySQLdb
+
+if __name__=="__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2], database=argv[3], port=3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
