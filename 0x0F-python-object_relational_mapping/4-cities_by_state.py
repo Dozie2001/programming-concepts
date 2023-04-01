5a6130a026226bfc65fb6691f757f18b65561f0d
Dozie2001 <davidchidozie402@gmail.com>
2023-04-01 19:50:55 +0100

    Correction to code base

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT c.id, c.name, s.name FROM cities AS c INNER JOINS
-                states AS s ON c.state_id = s.id""")
-
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
