794c3e32fec6961206e2a7d430f93dd13bea0697
Dozie2001 <davidchidozie402@gmail.com>
2023-04-04 07:29:33 +0100

    New file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,8 @@ from sqlalchemy import (create_engine)
 from model_state import Base, State
 
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
+engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                       format(argv[1], argv[2], argv[3]))
 
 Base.metadata.create_all(engine)
 
