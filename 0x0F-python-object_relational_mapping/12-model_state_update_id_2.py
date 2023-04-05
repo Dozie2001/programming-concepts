1d78646023feae95d735d822a64980f2c7458241
Dozie2001 <davidchidozie402@gmail.com>
2023-04-05 07:28:29 +0100

    Changed permission on file

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -2,7 +2,7 @@
 """A script that  changes the name of a State object 
 from the database hbtn_0e_6_usa
 """
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -10,10 +10,10 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance = session.query(State).filter(State.id == 2).first()
     new_instance.name = 'New Mexico'
     session.commit()
