a822e11794564f6d4cc28ad9c96e85ccc7972215
Dozie2001 <davidchidozie402@gmail.com>
2023-04-05 09:04:47 +0100

    New file

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """ prints the State object with the name passed as argument from the database
 """
-from sys import argv
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+import sys
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-if __name_ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session= Session()
-
-    for instance in session.query(State.name, City.id, City.name)
-                                  .filter(State.id == City.state_id).order_by(City.id):
-        print(f"{instance[0]}: {instance[1]} {instance[2]}")
+    session = Session()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
