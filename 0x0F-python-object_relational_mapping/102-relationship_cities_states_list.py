76adf1cfdc91909a9ca93c0342f15168f59908b8
Dozie2001 <davidchidozie402@gmail.com>
2023-04-13 07:28:09 +0100

    File

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""A script that lists all City objects from the
-database"""
-from sys import argv
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    engine = creat_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                          .format(argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
